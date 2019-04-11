d94e52d536df0823a525c6daae235989367aa416
Olivier Martineau <omartineau8@gmail.com>
2019-04-11 15:35:22 -0400

    adding updated styling changes

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-// Requiring necessary npm packages
-var express = require("express");
-var bodyParser = require("body-parser");
-var session = require("express-session");
-var exphbs = require("express-handlebars");
-var path = require("path");
+
+const express = require('express');
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const path = require("path");
+
 // Requiring passport as we've configured it
-var passport = require("./config/passport");
+const passport = require("./config/passport");
+
 // Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
+
 // Creating express app and configuring middleware needed for authentication
-var app = express();
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+
 //handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
